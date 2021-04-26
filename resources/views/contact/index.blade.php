2bb7b463482f160dd6e8665244c27b3866ebfe26
agrado <augusto.grado@bancamiga.com>
2021-04-26 09:16:44 -0400

    Cambio de color de título contact

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="col-12 col-sm-10 col-lg-10 mx-auto">
 				<div class="d-flex justify-content-between align-items-center mb-3">
-                    <h2 class="mb-0">Contactos</h2>
+                    <h2 class="text-primary mb-0">Contactos</h2>
                     @auth
                         <div class="btn-group btn-group-sm">
                             <a class="btn btn-outline-success"
