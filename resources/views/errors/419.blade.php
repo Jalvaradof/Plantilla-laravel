c11b5035b5918f53b68a7e58055488e3007357f4
agrado <augusto.grado@bancamiga.com>
2021-04-26 09:22:35 -0400

    Agregar archivos de errores

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<title>@yield('title', config('app.nick_name') . ' - 419')</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js" defer></script>
+    <script src="{{ asset('js/script.js') }}" defer=""></script>
+    <link  rel="icon"   href="/img/logoc.png" type="image/png"/>
+</head>
+<body>
+<br><br><br>
+	<div class="container">
+		<div class="row align-items-end">
+			<div class="col">
+				<h1 class="display-4 text-primary text-center font-weight-bold">419</h1>
+				<h3 class=" text-primary text-center">Tiempo expirado.</h3>
+				<img class="mx-auto d-block" src="/img/page_419.svg" alt="Control de Visitas" width="60%">
+				<a class="btn btn-link btn-block" href="/">Volver al inicio</a>
+			</div>
+
+		</div>
+	</div>
+</body>
+</html>
